d595c84e2a76d758fc4e4c0d09dcd52c717841d4
yourloff <alex.yourloff@gmail.com>
2025-03-02 12:50:39 +0700

    Степень тройки

---
+++ java_basics_power3_exercise/src/main/java/org/palindrome/Main.java
@@ -0,0 +1,30 @@
+package org.palindrome;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(0)); // false
+        System.out.println(isPowerOfThree(1)); // true
+        System.out.println(isPowerOfThree(3)); // true
+        System.out.println(isPowerOfThree(6)); // false
+        System.out.println(isPowerOfThree(9)); // true
+        System.out.println(isPowerOfThree(27)); // true
+        System.out.println(isPowerOfThree(81)); // true
+        System.out.println(isPowerOfThree(111)); // false
+    }
+
+    public static Boolean isPowerOfThree(int number) {
+        int i = 0;
+
+        while (i < number) {
+            int pow = (int) Math.pow(3, i);
+
+            if (pow == number) {
+                return true;
+            } else if(pow > number) {
+                return false;
+            }
+            i++;
+        }
+        return false;
+    }
+}
